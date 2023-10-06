8f3547dfcae625437bcd6cf2141d6551184fdb6a
dimasmnat <114051480+dimasmnat@users.noreply.github.com>
2023-10-05 17:19:50 -0700

    practica 11

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:practica_11/shared_page.dart';
+
+void main() => runApp( MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Practica 11',
+      home: SharedPage(),
+
+    );
+  }
+} 
+
+
